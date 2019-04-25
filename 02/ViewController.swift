e84bbd67051f2bf569bacf147d2f943b45adb21f
dit08 <dit@imac-3.local>
2019-04-25 16:40:52 +0900

    아이폰 앱개발 중간고사 02번 문제

--- 02/ViewController.swift
+++ 02/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    var color = ["흰색","초록","노랑"]
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lbl.text = color[0]
     }
-
-
+    @IBAction func btn(_ sender: Any) {
+        if count != 2 {
+            count += 1
+        } else {
+            count = 0
+        }
+        
+        if count == 0 {
+            view.backgroundColor = UIColor.white
+        } else if count == 1 {
+            view.backgroundColor = UIColor.green
+        } else {
+            view.backgroundColor = UIColor.yellow
+        }
+        
+        lbl.text = color[count]
+    }
 }
-
